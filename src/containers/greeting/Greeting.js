72d75656257ee13d8c92ac2a76be08d74c0d7efb
Sun flower <MLIU2@LTU.EDU>
2025-07-13 19:25:14 -0400

    fix resume path issue

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,9 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="MingyueLiu Resume.pdf"
+                    // href={require("./resume.pdf")}
+                    href={greeting.resumeLink}
+                    download="MingyueLiu_Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
